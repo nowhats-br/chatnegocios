01579b8857a1986909baf757aec17565c7215651
brazziltec-rgb <brazziltec@gmail.com>
2025-10-21 23:13:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: ["evochat.nowhats.com.br"],
+  },
+  preview: {
+    allowedHosts: ["evochat.nowhats.com.br"],
+  },
 })
